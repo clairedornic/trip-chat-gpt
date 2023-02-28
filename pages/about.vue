f44face2dfc555208770c723844f36d92137af9d
clairedornic <50169132+clairedornic@users.noreply.github.com>
2023-02-28 23:06:54 +0100

    FEAT: clean about vue

--- pages/about.vue
+++ pages/about.vue
@@ -1,19 +1,7 @@
 <template>
     <h1>About</h1>
-<div>
-  {{ count }}
-  <input v-model="inputVal" />
-  <button @click="increment">+</button>
-</div>
 </template>
 
 <script setup>
-  import { useCounter } from '@/store/counter'
-  import { storeToRefs } from 'pinia'
-
-  const inputVal = ref('')
-
-  const counterStore = useCounter()
-  const { increment } = counterStore
-  const { count } = storeToRefs(counterStore)
+ 
 </script>
