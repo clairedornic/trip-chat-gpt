3862cc1849ecc5b622bef185dfe1610b63d914dd
clairedornic <50169132+clairedornic@users.noreply.github.com>
2023-03-08 18:36:43 +0100

    FEAT: add about page view

--- pages/about.vue
+++ pages/about.vue
@@ -1,7 +1,9 @@
 <template>
-    <h1>About</h1>
+    <h1>{{ data.aboutPage.title }}</h1>
+    <p>{{ data.aboutPage.subtitle }}</p>
 </template>
 
 <script setup>
- 
+   import about from '@/cms/queries/about';
+    const { data: data, pending: postsPending } = await useLazyAsyncQuery(about)
 </script>
