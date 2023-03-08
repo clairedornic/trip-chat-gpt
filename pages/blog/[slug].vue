5d3f27db5647f9c71b579a711afac5f8b040e620
clairedornic <50169132+clairedornic@users.noreply.github.com>
2023-03-08 18:36:25 +0100

    FEAT: add archive page and single page for blog category

---
+++ pages/blog/[slug].vue
@@ -0,0 +1,16 @@
+<template>
+    <h1>{{ data.article.title }}</h1>
+    <p>{{ data.article.subtitle }}</p>
+    <p>{{ data.article._createdAt }}</p>
+    {{ data }}
+</template>
+
+<script setup>
+import article from '@/cms/queries/article';
+
+const route = useRoute()
+let slug = route.params.slug
+console.log( this.$route );
+
+const { data: data, pending: postsPending } = await useLazyAsyncQuery(article, { slug: {eq : slug } } );
+</script>
