4682699187705327d1bd80c7772f16247ce1a973
clairedornic <50169132+clairedornic@users.noreply.github.com>
2023-03-08 18:35:31 +0100

    FEAT: add components

---
+++ components/BestDestinations.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="articles-container">
+        <ul v-for="article in dataBestDestinations.allArticles" :key="article.id">
+            <li>
+                <nuxt-link :to="{name: 'destinations-slug', params: {slug: article.slug}}">
+                    <img :src="article.image.url" :alt="article.image.alt" :srcset="article.image.responsiveImage.scrSet">
+                    <h3>{{ article.title }}</h3>
+                    <p>{{ article.subtitle }}</p>
+                </nuxt-link>
+            </li>   
+        </ul>
+    </div>
+</template>
+<script setup>
+    import bestDestinations from '@/cms/queries/best-destinations';
+
+    const props = defineProps({
+        categoryId: String
+    })
+
+    const { data: dataBestDestinations, pending: bestDestinationsPending, error: error } = await useLazyAsyncQuery(bestDestinations, { idCategory: props.categoryId } );
+</script>
--- components/Header.vue
+++ components/Header.vue
@@ -1,16 +1,19 @@
 <template>
     <header>
 		<NuxtLink class="logo" to="/">
-            <img :src="data.header.logo.url" :alt="data.header.logo.alt">
+            <img :src="data.header.logo.url" :alt="data.header.logo.alt" :srcset="data.header.logo.responsiveImage.scrSet">
         </NuxtLink>
 		<nav>
 			<ul>
                 <template v-for="item_nav in data.header.navigationItems" :key="item_nav.id">
-                    <li v-if="item_nav.link._modelApiKey === 'category' ">
-                        <nuxt-link :to="{name: 'category-slug', params: {slug: item_nav.slug}}">{{ item_nav.title }}</nuxt-link>
+                    <li v-if="item_nav.slug === 'blog'">
+                        <NuxtLink :to="{name: 'blog'}">{{ item_nav.title }}</NuxtLink>
+                    </li>
+                    <li v-else-if="item_nav.slug === 'destinations'">
+                        <NuxtLink :to="{name: 'destinations'}">{{ item_nav.title }}</NuxtLink>
                     </li>
                     <li v-else>
-                        <nuxt-link :to="{name: 'slug', params: {slug: item_nav.slug}}">{{ item_nav.title }}</nuxt-link>
+                        <NuxtLink :to="{name: 'about'}">{{ item_nav.title }}</NuxtLink>
                     </li>
                 </template>
             </ul>
