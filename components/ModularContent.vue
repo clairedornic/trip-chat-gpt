dd5d7604d2967db180017c944ccaac20a2575c43
clairedornic <50169132+clairedornic@users.noreply.github.com>
2023-03-16 13:25:31 +0100

    FEAT: add new component for modular content

---
+++ components/ModularContent.vue
@@ -0,0 +1,40 @@
+<template>
+     <template v-for="contentElement in props.content.value.document.children">
+            <template v-if="contentElement.type === 'heading' && contentElement.level === 2">
+                <h2 v-for="title in contentElement.children">
+                    {{ title.value }}
+                </h2>
+            </template>
+            <template v-if="contentElement.type === 'heading' && contentElement.level === 3">
+                <h3 v-for="title in contentElement.children">
+                    {{ title.value }}
+                </h3>
+            </template>
+            <template v-if="contentElement.type === 'heading' && contentElement.level === 4">
+                <h4 v-for="title in contentElement.children">
+                    {{ title.value }}
+                </h4>
+            </template>
+            <template v-if="contentElement.type === 'paragraph'">
+                <p v-for="paragraph in contentElement.children">
+                    {{ paragraph.value }}
+                </p>
+            </template>
+            <ul v-if="contentElement.type === 'list'">
+                <li v-for="item in contentElement.children">
+                    <template v-for="itemList in item.children">
+                        <template v-for="itemListContent in itemList.children">
+                            {{ itemListContent.value }}
+                        </template>
+                    </template>
+                </li>
+            </ul>
+        </template>
+</template>
+<script setup>
+
+    const props = defineProps({
+        content : Object
+    })
+
+</script>
